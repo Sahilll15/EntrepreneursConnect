786c4e4abb0f482e0e111e1c9bf4082c374fef75
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-09-05 22:29:54 +0530

    group

--- client/src/store.js
+++ client/src/store.js
@@ -2,17 +2,19 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import authReducer from './redux/auth/authSlice';
 import postReducer from './redux/posts/postSlice'
 import likeReducer from './redux/likes/likeSlice'
+import CommunityReducer from './redux/community/CommunitySlice'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger';
 
-// const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [...getDefaultMiddleware(), logger];
 const store = configureStore({
     reducer: {
         user: authReducer,
         posts: postReducer,
-        likes: likeReducer
+        likes: likeReducer,
+        community: CommunityReducer
     },
-    // middleware
+    middleware
 
 
 })
