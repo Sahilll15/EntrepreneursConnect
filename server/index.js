73e94d270c2872b242408e7a4ff02fd0824097ce
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-09-20 16:56:40 +0530

    backend cors

--- server/index.js
+++ server/index.js
@@ -1,32 +1,36 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const app = express()
-const cors = require('cors')
-const socketIo = require('socket.io');
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
 const http = require('http');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const userRoutes = require('./routes/user.routes')
-const productsRoutes = require('./routes/Product.routes')
-const likeRoutes = require('./routes/like.routes')
-const commentRoutes = require('./routes/comments.routes')
-const refrealsRoutes = require('./routes/refreals.routes')
-const groupRoutes = require('./routes/group.routes')
-const notificationRoutes = require('./routes/notification.routes')
-const myDb = require('./db')
-
+const userRoutes = require('./routes/user.routes');
+const productsRoutes = require('./routes/Product.routes');
+const likeRoutes = require('./routes/like.routes');
+const commentRoutes = require('./routes/comments.routes');
+const refrealsRoutes = require('./routes/refreals.routes');
+const groupRoutes = require('./routes/group.routes');
+const notificationRoutes = require('./routes/notification.routes');
+const myDb = require('./db');
 
 const Server = http.createServer(app);
-const io = socketIo(Server);
 
-console.log(PORT)
+console.log(PORT);
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}`)
-})
+// Define a CORS middleware to allow requests only from a specific origin
+const corsOptions = {
+    origin: 'https://entrepreneursconnect.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+
+app.use(cors(corsOptions));
+
+Server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 myDb.myDb();
 app.use('/uploads', express.static('uploads'));
@@ -40,13 +44,6 @@ app.use('/api/v1/notifications', notificationRoutes);
 
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
     res.render('index');
 });
-
-
-io.on('connection', (socket) => {
-    console.log("user connected")
-}
-);
